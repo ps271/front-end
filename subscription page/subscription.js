b5814a253d56ff91ec7260b4a06fa4d61935697c
ps271 <53075019+ps271@users.noreply.github.com>
2021-09-19 21:19:27 +0530

    Add files via upload

---
+++ subscription page/subscription.js
@@ -0,0 +1,12 @@
+function changeColorSignUp(id) {
+    document.getElementById(id).style.color = "blue";
+    document.getElementById(id).style.backgroundColor = "hsl(150, 100%, 50%)";
+}
+
+function MouseMoveON(id) {
+    document.getElementById(id).style.backgroundColor = "hsl(30, 100%, 50%)"
+}
+
+function MouseMoveOut(id) {
+    document.getElementById(id).style.backgroundColor = "hsl(71, 73%, 54%)"
+}
